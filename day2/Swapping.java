adf8a747a5f2e725a73f35d4d02d8f850779608e
CAS EXAM <casexam@kgisledu.com>
2025-06-06 12:13:47 +0530

    changes

--- day2/Swapping.java
+++ day2/Swapping.java
@@ -1,5 +1,21 @@
 package day2;
 
 public class Swapping {
-
-}
+	    public static void main(String[] args) {
+	        int[] numbers = {4, 2, 10, 7, 5, 3, 8};
+	        for (int i = 0; i < numbers.length; i++) {
+	            int maxIndex = i;
+	            for (int j = i + 1; j < numbers.length; j++) {
+	                if (numbers[j] > numbers[maxIndex]) {
+	                    maxIndex = j;
+	                }
+	            }
+	            int temp = numbers[maxIndex];
+	            numbers[maxIndex] = numbers[i];
+	            numbers[i] = temp;
+	        }
+	        for (int num : numbers) {
+	            System.out.print(num + " ");
+	        }
+	    }
+	}
